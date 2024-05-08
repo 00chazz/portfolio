afdc552d5d59947c51c73b357ca0814c93ca2e2b
00chazz <chazzconino@gmail.com>
2024-05-08 03:56:45 -0500

    init commit

---
+++ src/components/intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Intro() {
+    return(
+        <div className ="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className ="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Chazz Conino</h1>
+            <p className ="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
+            <p className = "text-sm max-w-xl mb-6 font-bold">I am just a small town girl living in a lonely world. And I'm a Barbie Girl in a Barbie World. <br></br></p>
+            <p className = "font-extrabold">Now it's your turn.</p>
+            </div>
+    )
+}
+
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Full Stack Library Manager',
+        imgUrl: '/assets/luxury-hero.jpg',
+        stack: ['Java', 'Spring Boot', 'Angular', 'React', 'MySQL'],
+        link: '',
+    },
+    {
+        title: 'Delivery Route Optimization Program',
+        imgUrl: '/assets/rocket.jpg',
+        stack: ['JavaScript', 'Python', 'GraphQL', 'Node'],
+        link: '',
+    },
+    {
+        title: 'AI Project',
+        imgUrl: '/assets/real-ai.jpg',
+        stack: ['Python', 'AIML', 'TensorFlow'],
+        link: '',
+    }
+]
